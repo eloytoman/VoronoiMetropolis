21c2e75e95e87ed23b903e6e8497e7aef7d703d0
eloytoman <83752779+eloytoman@users.noreply.github.com>
2023-02-20 20:46:48 +0100

    energy analisis

--- Analysis/energy analysis.R
+++ Analysis/energy analysis.R
@@ -8,15 +8,29 @@ library(foreach)
 library(doParallel)
 
 
-energy_analysis <- function(points){
+# This functions computes the energy of a certain tessellation, given the
+# points that generate it. It makes a distinction between the different types 
+# of energies, (bending, elastic, tension and contractile). So this functions 
+# makes the energy decomposition in a given instant of the algorithm for every 
+# layer of the cylinder. The output is a dataframe which has the info of every
+# type of energy for all layers in the given instant.
+
+energy_analysis <- function(points, Lay){
   
   
+  #First we initialize the dataframe that store the results.
   tesener <- data.frame( Layer = seq(1, rad_coef, by = ((rad_coef-1)/(Lay-1))),
                          Elastic_energy = double(Lay),
                          Tension_energy = double(Lay),
                          Contractile_energy = double(Lay),
                          Bending_energy = double(Lay),
                          Total_energy=double(Lay))
+  
+  
+  # Now we apply the same computation that we do when computing the energy 
+  # during the algorithm, but storing every energy separately. To see more 
+  # details, see the function "tesellation_energy_N" in N_cylinder_algorithm.R 
+  # script
   
   for (i in 1:Lay) {
     tesel <- deldir(points[[i]]$x,points[[i]]$y,rw=rec[[i]])
@@ -37,6 +51,7 @@ energy_analysis <- function(points){
   
   }
   
+  #Same with the bending energy
   tesener$Bending_energy[c(1,Lay)]<-0
   
   tesener$Bending_energy[2:(Lay-1)] <- sapply(2:(Lay-1), function(i){
@@ -70,6 +85,92 @@ energy_analysis <- function(points){
   tesener$Total_energy <- tesener$Total_energy+tesener$Bending_energy
   return(tesener)
 }
+
+
+# Example of use of the function above for the results of the parallel algorithm
+# with bending, specifically, we use the 4th simulation and we take the
+# iteration number 550 (when the algorithm has reached equilibrium). We 
+# initialize the necessary constants and then we load the data from results.
+
+gamad <- 0.15
+lamad <- 0.04
+alpha<-0.05
+
+cyl_width <- 5
+cyl_length <- 20
+xmin <- 0
+ymin <- 0
+xmax <- cyl_width
+ymax <- cyl_length
+
+rad_coef<-2.5
+
+Radius <- cyl_width/(2*pi)
+Radius2 <- rad_coef*Radius
+cyl_width2 <- 2*pi*Radius2
+
+cyl_thickness <- Radius2-Radius
+
+n <- 100
+Lay <- 10  #Layers
+lay <- Lay
+r <- cyl_width/n 
+bet <- 10
+steps <- 550
+
+A0 <- (((Radius2+Radius)/2)*2*pi*cyl_length)/n
+
+rec <- list()
+rad <- list()
+for(k in 1:Lay){
+  rad[[k]]<- Radius+(k-1)*(cyl_thickness/(Lay-1)) #the radius of the layer k
+  rec[[k]]<-c(xmin,xmin+3*(2*pi*rad[[k]]),ymin,ymax)
+}
+
+points <- list(Lay)
+for (j in 1:Lay) {
+  points[[j]]<-filter(results[[4]][[j]],Frame==550)
+}
+
+ener<-energy_analysis(points)
+
+p<-ggplot(ener, aes(x = Layer))+
+  geom_line(aes(y = Total_energy, colour = "Total energy"))+
+  geom_line(aes(y = Tension_energy, colour = "Tension energy"))+
+  geom_line(aes(y = Elastic_energy,colour = "Adhesion energy"))+
+  geom_line(aes(y = Contractile_energy, colour = "Contractile energy"))+
+  geom_line(aes(x = Layer, y = Bending_energy, colour = "Bending energy"))+
+  labs(title = "Energy Analysis after 550 iterations of the algorithm with Bending. Omega=0.05",
+       x = "Layer Ratio (s=R/Ra)", y = "Average energy per cell",
+       color = "Energy type") +
+  scale_colour_manual("",
+                      breaks = c("Tension energy",
+                                 "Adhesion energy",
+                                 "Contractile energy",
+                                 "Bending energy",
+                                 "Total energy"),
+                      values = c("red",
+                                 "blue",
+                                 "darkgreen",
+                                 "orange",
+                                 "purple"))
+
+
+show(p)
+
+
+
+
+
+
+# Next function makes the energy decomposition in the first iteration and in 
+# the last iteration of the algorithm, so we can compare the initial random
+# distribution with the energy decomposition in an equilibrium state.
+# In this case, we
+# The function takes as input the histpts dataframe, which is the result of the
+# N_cylinder_algorithm. This version does not take into account the bending 
+# energy. The output are two graphics that show the energy decomposition 
+# by layers at the beginning and at the end of the algorithm.
 
 energy_analysis_nobend <- function(histpts, it=150, n=100, Lay=10){
   
@@ -169,6 +270,16 @@ energy_analysis_nobend <- function(histpts, it=150, n=100, Lay=10){
   return(list(tesener,tesener2))
 }
 
+
+
+# This function computes the value of every energy type for 1 iteration. The 
+# objective is measure in a single iteration the values of every energy
+# (tension, contractile, elastic and total energy).
+# This one does not support bending energy.
+# The difference with the first one is that the first one computes every energy
+# for every layer in one iteration, and this one computes every energy for all
+# layers together in one iteration (we sum energies of all layers)
+
 energy_iteration <- function(pointsx, pointsy, n=100, Lay = 10){
   
   
@@ -207,6 +318,13 @@ energy_iteration <- function(pointsx, pointsy, n=100, Lay = 10){
   return(c(elasticener,tensionener,contractilener,totalener))
 }
 
+
+
+# Given one whole simulation (we perform the algorithm twice and we store the 
+# results in the dataframe of points histpts), this function stores the energy
+# decomposition of every iteration (with the function above "energy_iteration").
+# Then it returns the dataframe with the decomposition of energies for every
+# iteration, having the historic of energy relaxation for every specific energy.
 energy_analisis_1sim <- function(histpts, it = 150){
   histener<-data.frame(it = 1:it, elen = double(it), tenen = double(it),
                        conten = double(it), toten = double(it))
@@ -218,9 +336,9 @@ energy_analisis_1sim <- function(histpts, it = 150){
   return(histener)
 }
 
-enerhist <- energy_analisis_1sim(hist1)
-plot_energyss(enerhist)
-
+# This functions takes as input the output of the function above 
+# "energy_analisis_1sim" and plot the energy relaxation for every specific
+# energy
 plot_energyss<-function(enerhist){
   
   p<-ggplot(enerhist, aes(x = it))+
@@ -236,17 +354,43 @@ plot_energyss<-function(enerhist){
                         breaks = c("Tension energy",
                                    "Adhesion energy",
                                    "Contractile energy"),
-                                   # "Total energy",
-                                   # "Bending energy"),
+                        # "Total energy",
+                        # "Bending energy"),
                         values = c("red",
                                    "blue",
                                    "darkgreen"))
-                                   # "orange",
-                                   # "purple"))
+  # "orange",
+  # "purple"))
   
   show(p)
 }
 
+# Example of use of the functions above with hist1, the output of the script
+# N_cylinder_algorithm.R
+enerhist <- energy_analisis_1sim(hist1)
+plot_energyss(enerhist)
+
+
+#This function computes the total energy of every layer in one iteration
+energy_1_layer <- function(pointsx, pointsy, rec, i, Lay =10, n=100){
+  
+  tesel <- deldir(pointsx,pointsy,rw=rec)
+  tilest <- tile.list(tesel)[(n+1):(2*n)]
+  
+  perims <- (tilePerim(tilest)$perimeters)/sqrt(A0)
+  areas <- sapply(tilest,function(x){x$area/A0})
+  
+  gam<-gamad*exp((1-(rad[[i]]/rad[[1]]))/1 )
+  
+  tesener<-sum((areas-1)^2+(gam/2)*(perims^2)+
+                 lamad*perims)/(Lay*n)
+  return(tesener)
+}
+
+# This function takes as input the historic of points (output of 
+# N_cylinder_algorithm) and returns the energy relaxation for every layer along
+# the execution of the algorithm. In this case we chose only 6 layers, because
+# the energy in intermediate layers is similar.
 energy_layers_sim1 <- function(histpts,it = 150){
   
     tesener <- data.frame( iter = 1:it,
@@ -270,21 +414,8 @@ energy_layers_sim1 <- function(histpts,it = 150){
     return(tesener)
 }
 
-energy_1_layer <- function(pointsx, pointsy, rec, i, Lay =10, n=100){
-  
-    tesel <- deldir(pointsx,pointsy,rw=rec)
-    tilest <- tile.list(tesel)[(n+1):(2*n)]
-    
-    perims <- (tilePerim(tilest)$perimeters)/sqrt(A0)
-    areas <- sapply(tilest,function(x){x$area/A0})
-    
-    gam<-gamad*exp((1-(rad[[i]]/rad[[1]]))/1 )
-    
-    tesener<-sum((areas-1)^2+(gam/2)*(perims^2)+
-                        lamad*perims)/(Lay*n)
-  return(tesener)
-}
-
+# This function plots the results of the function above, so we can visualize 
+# the energy relaxation by layers
 plot_energy_decomp<-function(tesenerdec){
   
 ploten <- ggplot(tesenerdec, aes(x = iter))+
@@ -313,77 +444,17 @@ ploten <- ggplot(tesenerdec, aes(x = iter))+
 
   show(ploten)
 }
- 
-
-gamad <- 0.15
-lamad <- 0.04
-alpha<-0.05
-
-cyl_width <- 5
-cyl_length <- 20
-xmin <- 0
-ymin <- 0
-xmax <- cyl_width
-ymax <- cyl_length
-
-rad_coef<-2.5
-
-Radius <- cyl_width/(2*pi)
-Radius2 <- rad_coef*Radius
-cyl_width2 <- 2*pi*Radius2
-
-cyl_thickness <- Radius2-Radius
-
-n <- 100
-Lay <- 10  #Layers
-lay <- Lay
-r <- cyl_width/n 
-bet <- 10
-steps <- 550
-
-A0 <- (((Radius2+Radius)/2)*2*pi*cyl_length)/n
-
-rec <- list()
-rad <- list()
-for(k in 1:Lay){
-  rad[[k]]<- Radius+(k-1)*(cyl_thickness/(Lay-1)) #the radius of the layer k
-  rec[[k]]<-c(xmin,xmin+3*(2*pi*rad[[k]]),ymin,ymax)
-}
-
-points <- list(Lay)
-for (j in 1:Lay) {
-  points[[j]]<-filter(results[[4]][[j]],Frame==550)
-}
-
-ener<-energy_analysis(points)
-
-p<-ggplot(ener, aes(x = Layer))+
-  geom_line(aes(y = Total_energy, colour = "Total energy"))+
-  geom_line(aes(y = Tension_energy, colour = "Tension energy"))+
-  geom_line(aes(y = Elastic_energy,colour = "Adhesion energy"))+
-  geom_line(aes(y = Contractile_energy, colour = "Contractile energy"))+
-  geom_line(aes(x = Layer, y = Bending_energy, colour = "Bending energy"))+
-  labs(title = "Energy Analysis after 550 iterations of the algorithm with Bending. Omega=0.05",
-       x = "Layer Ratio (s=R/Ra)", y = "Average energy per cell",
-       color = "Energy type") +
-  scale_colour_manual("",
-                     breaks = c("Tension energy",
-                                "Adhesion energy",
-                                "Contractile energy",
-                                "Bending energy",
-                                "Total energy"),
-                     values = c("red",
-                                "blue",
-                                "darkgreen",
-                                "orange",
-                                "purple"))
-
-
-show(p)
 
 
 
 
+
+# The next function is used after performing a search of the best parameters
+# as input for the algorithm, particulary changing the values of Omega.
+# The function takes as input the results of the parallel algorithm 
+# without bending (performed with different values of the Omega parameter) 
+# and plots the energy relaxations for every simulation (every value of the 
+# parameter)
 plotenergies<-function(results,it=551){
   en<-data.frame(iteration=1:it, en1=results[[9]]$energy/100,
                  en2=results[[10]]$energy/100, en3=results[[11]]$energy/100,
@@ -427,39 +498,17 @@ plotenergies<-function(results,it=551){
 }
 plotenergies(results = results)
 
-####BENDINGGGG
-
-bending_energy_layers_sim1 <- function(histpts,it =550){
-  
-  tesener <- data.frame( iter = 1:it,
-                         enlay1 = double(it),
-                         enlay2 = double(it),
-                         enlay3 = double(it),
-                         enlay4 = double(it),
-                         enlay5 = double(it),
-                         enlay6 = double(it),
-                         enlay7 = double(it),
-                         enlay8 = double(it),
-                         enlay9 = double(it),
-                         enlay10 = double(it))
-  
-  for (i in 2:it) {
-    ener<-rep(0,6)
-    k<-1
-    for (j in c(2,3,4,5,6,7,8,9)) {
-      ener[[k]] <- energy_1_layer_bending(histpts, rec[[j]], rad, i = j, it = i)
-      k<-k+1
-    }
-    tesener[i,c(3,4,5,6,7,8,9,10)] <- ener
-    points1 <- filter(histpts[[1]],Frame==i)
-    points10 <- filter(histpts[[10]],Frame==i)
-    tesener[i,2]<-energy_1_layer(points1$x,points1$y, rec[[1]],1)
-    tesener[i,11]<-energy_1_layer(points10$x,points10$y,rec[[10]],10)
-  }
-  return(tesener)
-}
 
 
+### BENDING FUNCTIONS
+
+
+
+# This function computes the total energy of one single layer for the algorithm
+# with bending. Here, "histpts" is the ouput of the bending algorithm,
+# "rect" is the border of the cylinder in the layer we want to compute the energy,
+# "rad" is the radius of the layer, "i" is the layer and "it" is the iteration of the 
+# algorithm
 energy_1_layer_bending <- function(histpts, rect, rad, i, it, Lay =10, n=100){
   
   points<-list(10)
@@ -500,11 +549,48 @@ energy_1_layer_bending <- function(histpts, rect, rad, i, it, Lay =10, n=100){
     ang <- acos(v)        
     return(ang)
   })
-    bendener <- sum(alpha*((angles-pi)^2))/(Lay*n)
+  bendener <- sum(alpha*((angles-pi)^2))/(Lay*n)
   
   return(tesener+bendener)
 }
 
+# This function stores decomposes the energy relaxation by layers,
+# so we have specifically the energy relaxation in every layer. As input, it 
+# takes the histpts list of dataframes which is output of the algorithm with
+# bending
+bending_energy_layers_sim1 <- function(histpts,it =550){
+  
+  tesener <- data.frame( iter = 1:it,
+                         enlay1 = double(it),
+                         enlay2 = double(it),
+                         enlay3 = double(it),
+                         enlay4 = double(it),
+                         enlay5 = double(it),
+                         enlay6 = double(it),
+                         enlay7 = double(it),
+                         enlay8 = double(it),
+                         enlay9 = double(it),
+                         enlay10 = double(it))
+  
+  for (i in 2:it) {
+    ener<-rep(0,6)
+    k<-1
+    for (j in c(2,3,4,5,6,7,8,9)) {
+      ener[[k]] <- energy_1_layer_bending(histpts, rec[[j]], rad, i = j, it = i)
+      k<-k+1
+    }
+    tesener[i,c(3,4,5,6,7,8,9,10)] <- ener
+    points1 <- filter(histpts[[1]],Frame==i)
+    points10 <- filter(histpts[[10]],Frame==i)
+    tesener[i,2]<-energy_1_layer(points1$x,points1$y, rec[[1]],1)
+    tesener[i,11]<-energy_1_layer(points10$x,points10$y,rec[[10]],10)
+  }
+  return(tesener)
+}
+
+
+# This function plots the energy relaxations for every layer. It takes as input
+# the output of the previous function.
 plot_energy_decomp_bending<-function(tesenerdec){
   
   ploten <- ggplot(tesenerdec, aes(x = iter))+
@@ -547,6 +633,8 @@ plot_energy_decomp_bending<-function(tesenerdec){
 }
 
 
+#Example of using the previous functions with one simulation performed with 
+# the parallel algorithm (the fourth simulation)
 
 hist1<-results[[4]]
 tes<-bending_energy_layers_sim1(hist1)
@@ -554,6 +642,10 @@ tes<-bending_energy_layers_sim1(hist1)
 plot_energy_decomp_bending(tes)
 
 
+
+# This computes the energy decomposition by energy type in a single iteration
+# and returns the energy of every type in the iteration. It does not make 
+# distinctions by layer (all are sumed up)
 energy_iteration_bending <- function(histpts, it, n=100, Lay = 10){
   
   points<-list(Lay)
@@ -620,7 +712,7 @@ energy_iteration_bending <- function(histpts, it, n=100, Lay = 10){
   return(c(elasticener,tensionener,contractilener,bendener,totalener))
 }
 
-
+# This function computes the energy relaxation for every energy type
 energy_analisis_1sim_bending <- function(histpts, it = 300){
   histener<-data.frame(it = 1:it, elen = double(it), tenen = double(it),
                        conten = double(it), benden = double(it),
@@ -632,7 +724,7 @@ energy_analisis_1sim_bending <- function(histpts, it = 300){
   return(histener)
 }
 
-
+# This function plots the results from the previous function
 plot_energyss_bending<-function(enerhist){
   
   p<-ggplot(enerhist, aes(x = it))+
@@ -661,12 +753,17 @@ plot_energyss_bending<-function(enerhist){
 }
 
 
+# Example with the data that we have from before
+
 enerben <- energy_analisis_1sim_bending(hist1, 550)
 plot_energyss_bending(enerben)
 
 
 
-
+# Returns the total energy with bending of a certain tessellation (this is the 
+# function used in the algorithm). The input is a list of dataframes, 
+# each dataframe being the points in the iteration for a certain layer (each
+# layer is stored in each element of the list)
 bending_tesellation_energy_N <- function(points){
   
   tesener<-numeric(L)
@@ -707,6 +804,11 @@ bending_tesellation_energy_N <- function(points){
   return((sum(tesener)+sum(bendener))/L)
 }
 
+
+# Example of using the the function above to compute the energy in every 
+# iteration after performing the algorithm with bending (recall that this
+# algorithm have as output a list of dataframes, every dataframe being the
+# energy relaxation for a layer)
 enertes <- data.frame(iteration=3:550, energy= double(548))
 for(k in 3:550){
   points<-list(10)
